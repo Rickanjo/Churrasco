53102c8ff09ca7439eba6f4e7c0c978ad5679fe9
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-22 12:28:21 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Faça um programa que calcule a quantidade de itens necessários para um churrasco a partir da quantidade de adultos e crianças
+
+Console.WriteLine("--- Churas sem desperdicio ---");
+
+Console.Write("Quantidade de adultos que consomem bebidas alcoólicas.............: ");
+int adultosAlcool = int.Parse(Console.ReadLine()!);
+
+Console.Write("Quantidade de adultos que não consomem bebidas alcoólicas.........: ");
+int adultosNaoAlcool = int.Parse(Console.ReadLine()!);
+
+Console.Write("Quantidade de crianças............................................: ");
+int crianças = int.Parse(Console.ReadLine()!);
+
+int adultos = adultosAlcool + adultosNaoAlcool;
+int pessoas = adultos + crianças;
+
+double carne = (adultos * 0.4) + 0.2 * crianças;
+double acompanhamentos = pessoas * 0.2;
+double cerveja = adultosAlcool * 2;
+double refrigerante = adultosNaoAlcool + crianças * 0.5;
+double agua = pessoas * 0.4;
+
+Console.WriteLine($"\nCarne................: {carne:N1}kg");
+Console.WriteLine($"Acompanhamentos........: {acompanhamentos:N1}kg"); 
+Console.WriteLine($"Cerveja................: {cerveja:N1}L"); 
+Console.WriteLine($"Refrigerante...........: {refrigerante:N1}L"); 
+Console.WriteLine($"Água...................: {agua:N1}L");
+
+
